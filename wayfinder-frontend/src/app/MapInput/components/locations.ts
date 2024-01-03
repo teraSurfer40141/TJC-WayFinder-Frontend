103c94aa927090a4d2d32d6b9e5cb413e1f05d8c
alexilawrence <131229920+alexilawrence@users.noreply.github.com>
2024-01-03 13:42:54 +0800

    Update locations.ts

--- wayfinder-frontend/src/app/MapInput/components/locations.ts
+++ wayfinder-frontend/src/app/MapInput/components/locations.ts
@@ -7,268 +7,772 @@ export const schlocations: schLocation[] = [{
   "value": "main entrance", // lowercase everything of the label
   "code": "A1", // that will be used in the backend
   "label": "Main Entrance" // that will show on the website
-}, {
-  "value": "a stairs",
-  "code": "AS",
-  "label": "A Stairs"
-}, {
-  "value": "4a",
-  "code": "B101",
-  "label": "4A"
-}, {
-  "value": "4b",
-  "code": "B102",
-  "label": "4B"
-}, {
-  "value": "4c",
-  "code": "B103",
-  "label": "4C"
-}, {
-  "value": "4d",
-  "code": "B104",
-  "label": "4D"
-}, {
-  "value": "4e",
-  "code": "B105",
-  "label": "4E"
-}, {
-  "value": "4f",
-  "code": "B106",
-  "label": "4F"
-}, {
-  "value": "art room 1",
-  "code": "B107a",
-  "label": "ART Room 1"
-}, {
-  "value": "art room 2",
-  "code": "B107b",
-  "label": "ART Room 2"
-}, {
-  "value": "art room store",
-  "code": "B107c",
-  "label": "ART Room Store"
-}, {
-  "value": "j1-1",
-  "code": "B108",
-  "label": "J1-1"
-}, {
-  "value": "j1-2",
-  "code": "B109",
-  "label": "J1-2"
-}, {
-  "value": "j1-3",
-  "code": "B110",
-  "label": "J1-3"
-}, {
-  "value": "b stairs (left)",
-  "code": "BSL",
-  "label": "B Stairs (Left)"
-}, {
-  "value": "b stairs (right)",
-  "code": "BSR",
-  "label": "B Stairs (Right)"
-}, {
-  "value": "b girls (left)",
-  "code": "BGL",
-  "label": "B Girls (Left)"
-}, {
-  "value": "b girls (right)",
-  "code": "BGR",
-  "label": "B Girls (Right)"
-}, {
-  "value": "b boys (left)",
-  "code": "BBL",
-  "label": "B Boys (Left)"
-}, {
-  "value": "b boys (right)",
-  "code": "BBR",
-  "label": "B Boys (Right)"
-}, {
-  "value": "canteen",
-  "code": "Canteen",
-  "label": "Canteen"
-}, {
-  "value": "court stairs (next to canteen)",
-  "code": "CSC",
-  "label": "Court Stairs (next to canteen)"
-}, {
-  "value": "1a homeroom",
-  "code": "D101",
-  "label": "1A Homeroom"
-}, {
-  "value": "1b homeroom",
-  "code": "D102",
-  "label": "1B Homeroom"
-}, {
-  "value": "1c homeroom",
-  "code": "D103",
-  "label": "1C Homeroom"
-}, {
-  "value": "1d homeroom",
-  "code": "D104",
-  "label": "1D Homeroom"
-}, {
-  "value": "1e homeroom",
-  "code": "D105",
-  "label": "1E Homeroom"
-}, {
-  "value": "2a homeroom",
-  "code": "D106",
-  "label": "2A Homeroom"
-}, {
-  "value": "handi + fem (blk d)",
-  "code": "DT",
-  "label": "Handi + Fem (Blk D)"
-},  {
-  "value": "j1-4",
-  "code": "E101",
-  "label": "J1-4"
-}, {
-  "value": "j1-6",
-  "code": "E102",
-  "label": "J1-6"
-}, {
-  "value": "j1-7",
-  "code": "E103",
-  "label": "J1-7"
-}, {
-  "value": "j1-8",
-  "code": "E104",
-  "label": "J1-8"
-}, {
-  "value": "j1-9",
-  "code": "E105",
-  "label": "J1-9"
-}, {
-  "value": "j1-10",
-  "code": "E106",
-  "label": "J1-10"
-}, {
-  "value": "eas",
-  "code": "E107",
-  "label": "EAS"
-}, {
-  "value": "student services hub",
-  "code": "E108",
-  "label": "Student Services Hub"
-}, {
-  "value": "j1-11",
-  "code": "E109",
-  "label": "J1-11"
-}, {
-  "value": "info ctr",
-  "code": "E110",
-  "label": "INFO CTR"
-}, {
-  "value": "temasek lounge",
-  "code": "E111",
-  "label": "Temasek Lounge"
-}, {
-  "value": "wht",
-  "code": "E112",
-  "label": "WHT"
-}, {
-  "value": "e girls (up)",
-  "code": "EGU",
-  "label": "E Girls (Up)"
-}, {
-  "value": "e boys (up)",
-  "code": "EBU",
-  "label": "E Boys (Up)"
-}, {
-  "value": "e girls (down)",
-  "code": "EGD",
-  "label": "E Girls (Down)"
-}, {
-  "value": "e boys (down)",
-  "code": "EBD",
-  "label": "E Boys (Down)"
-}, {
-  "value": "e stairs (left)",
-  "code": "ESL",
-  "label": "E Stairs (Left)"
-}, {
-  "value": "e stairs (right)",
-  "code": "ESR",
-  "label": "E Stairs (Right)"
-},  {
-  "value": "student council",
-  "code": "G101",
-  "label": "Student Council"
-}, {
-  "value": "na",
-  "code": "G102",
-  "label": "NA"
-}, {
-  "value": "na",
-  "code": "G103",
-  "label": "NA"
-}, {
-  "value": "pe staff room",
-  "code": "G104",
-  "label": "PE Staff Room"
-}, {
-  "value": "printing room",
-  "code": "G105",
-  "label": "Printing Room"
-}, {
-  "value": "bookshop room",
-  "code": "G106",
-  "label": "Bookshop Room"
-}, {
-  "value": "cafe",
-  "code": "GC",
-  "label": "Cafe"
-}, {
-  "value": "spare cr",
-  "code": "J101",
-  "label": "Spare CR"
-}, {
-  "value": "seminar room",
-  "code": "J102",
-  "label": "Seminar Room"
-}, {
-  "value": "spare cr",
-  "code": "J103",
-  "label": "Spare CR"
-}, {
-  "value": "j1-12",
-  "code": "J104",
-  "label": "J1-12"
-}, {
-  "value": "clep",
-  "code": "J105",
-  "label": "CLEP"
-}, {
-  "value": "mtl room",
-  "code": "J106",
-  "label": "MTL Room"
-}, {
-  "value": "mtl room",
-  "code": "J107",
-  "label": "MTL Room"
-},  {
-  "value": "f toilets l",
-  "code": "FT1",
-  "label": "F Toilets L"
-}, {
-  "value": "f toilets r",
-  "code": "FT2",
-  "label": "F Toilets R"
-},  {
-  "value": "amphitheatre",
-  "code": "HA",
-  "label": "AMPHITHEATRE"
-}, {
-  "value": "walkway l",
-  "code": "HWL",
-  "label": "Walkway L"
-}, {
-  "value": "walkway r",
-  "code": "HWR",
-  "label": "Walkway R"
-}, {
-  "value": "seating gallery",
-  "code": "HSG",
-  "label": "Seating Gallery"
-  }, ]
+},
+{"value": "l1 a stairs", 
+"code": "AS", 
+"label": "L1 A Stairs" 
+},
+{"value": "b101", 
+"code": "B101", 
+"label": "B101" 
+},
+{"value": "b102", 
+"code": "B102", 
+"label": "B102" 
+},
+{"value": "b103", 
+"code": "B103", 
+"label": "B103" 
+},
+{"value": "b104", 
+"code": "B104", 
+"label": "B104" 
+},
+{"value": "b105", 
+"code": "B105", 
+"label": "B105" 
+},
+{"value": "b106", 
+"code": "B106", 
+"label": "B106" 
+},
+{"value": "art room 1", 
+"code": "B107a", 
+"label": "ART Room 1" 
+},
+{"value": "art room 2", 
+"code": "B107b", 
+"label": "ART Room 2" 
+},
+{"value": "art room store", 
+"code": "B107c", 
+"label": "ART Room Store" 
+},
+{"value": "b108", 
+"code": "B108", 
+"label": "B108" 
+},
+{"value": "b109", 
+"code": "B109", 
+"label": "B109" 
+},
+{"value": "b110", 
+"code": "B110", 
+"label": "B110" 
+},
+{"value": "l1 b stairs (left)", 
+"code": "BSL", 
+"label": "L1 B Stairs (Left)" 
+},
+{"value": "l1 b stairs (right)", 
+"code": "BSR", 
+"label": "L1 B Stairs (Right)" 
+},
+{"value": "l1 b girls (left)", 
+"code": "BGL", 
+"label": "L1 B Girls (Left)" 
+},
+{"value": "l1 b girls (right)", 
+"code": "BGR", 
+"label": "L1 B Girls (Right)" 
+},
+{"value": "l1b boys (left)", 
+"code": "BBL", 
+"label": "L1B Boys (Left)" 
+},
+{"value": "l1 b boys (right)", 
+"code": "BBR", 
+"label": "L1 B Boys (Right)" 
+},
+{"value": "canteen", 
+"code": "Canteen", 
+"label": "Canteen" 
+},
+{"value": "court stairs (next to canteen)", 
+"code": "CSC", 
+"label": "Court Stairs (next to canteen)" 
+},
+{"value": "d101", 
+"code": "D101", 
+"label": "D101" 
+},
+{"value": "d102", 
+"code": "D102", 
+"label": "D102" 
+},
+{"value": "d103", 
+"code": "D103", 
+"label": "D103" 
+},
+{"value": "d104", 
+"code": "D104", 
+"label": "D104" 
+},
+{"value": "d105", 
+"code": "D105", 
+"label": "D105" 
+},
+{"value": "d106", 
+"code": "D106", 
+"label": "D106" 
+},
+{"value": "handi + fem toilets (blk d)", 
+"code": "DT", 
+"label": "Handi + Fem Toilets (Blk D)" 
+},
+{"value": "l1 d stairs", 
+"code": "DS1", 
+"label": "L1 D Stairs" 
+},
+{"value": "e101", 
+"code": "E101", 
+"label": "E101" 
+},
+{"value": "e102", 
+"code": "E102", 
+"label": "E102" 
+},
+{"value": "e103", 
+"code": "E103", 
+"label": "E103" 
+},
+{"value": "e104", 
+"code": "E104", 
+"label": "E104" 
+},
+{"value": "e105", 
+"code": "E105", 
+"label": "E105" 
+},
+{"value": "e106", 
+"code": "E106", 
+"label": "E106" 
+},
+{"value": "e107", 
+"code": "E107", 
+"label": "E107" 
+},
+{"value": "student services hub", 
+"code": "E108", 
+"label": "Student Services Hub" 
+},
+{"value": "e109", 
+"code": "E109", 
+"label": "E109" 
+},
+{"value": "info ctr", 
+"code": "E110", 
+"label": "INFO CTR" 
+},
+{"value": "temasek lounge", 
+"code": "E111", 
+"label": "Temasek Lounge" 
+},
+{"value": "wht", 
+"code": "E112", 
+"label": "WHT" 
+},
+{"value": "l1 e girls (up)", 
+"code": "EGU", 
+"label": "L1 E Girls (Up)" 
+},
+{"value": "l1 e boys (up)", 
+"code": "EBU", 
+"label": "L1 E Boys (Up)" 
+},
+{"value": "l1 e girls (down)", 
+"code": "EGD", 
+"label": "L1 E Girls (Down)" 
+},
+{"value": "l1 e boys (down)", 
+"code": "EBD", 
+"label": "L1 E Boys (Down)" 
+},
+{"value": "l1 e stairs (left)", 
+"code": "ESL", 
+"label": "L1 E Stairs (Left)" 
+},
+{"value": "l1 e stairs (right)", 
+"code": "ESR", 
+"label": "L1 E Stairs (Right)" 
+},
+{"value": "student council room", 
+"code": "G101", 
+"label": "Student Council Room" 
+},
+{"value": "g102", 
+"code": "G102", 
+"label": "G102" 
+},
+{"value": "g103", 
+"code": "G103", 
+"label": "G103" 
+},
+{"value": "pe staff room", 
+"code": "G104", 
+"label": "PE Staff Room" 
+},
+{"value": "printing room", 
+"code": "G105", 
+"label": "Printing Room" 
+},
+{"value": "bookshop room", 
+"code": "G106", 
+"label": "Bookshop Room" 
+},
+{"value": "cafe", 
+"code": "GC", 
+"label": "Cafe" 
+},
+{"value": "j101", 
+"code": "J101", 
+"label": "J101" 
+},
+{"value": "seminar room", 
+"code": "J102", 
+"label": "Seminar Room" 
+},
+{"value": "j103", 
+"code": "J103", 
+"label": "J103" 
+},
+{"value": "j104", 
+"code": "J104", 
+"label": "J104" 
+},
+{"value": "clep", 
+"code": "J105", 
+"label": "CLEP" 
+},
+{"value": "j106", 
+"code": "J106", 
+"label": "J106" 
+},
+{"value": "j107", 
+"code": "J107", 
+"label": "J107" 
+},
+{"value": "l1 f toilets l", 
+"code": "FT1", 
+"label": "L1 F Toilets L" 
+},
+{"value": "l1 f toilets r", 
+"code": "FT2", 
+"label": "L1 F Toilets R" 
+},
+{"value": "amphitheatre", 
+"code": "HA", 
+"label": "AMPHITHEATRE" 
+},
+{"value": "seating gallery", 
+"code": "HSG", 
+"label": "Seating Gallery" 
+},
+ {"value": "humans cr1", 
+  "code": "A01LG", 
+  "label": "Humans CR1" 
+ },
+ {"value": "humans cr2", 
+  "code": "A02LG", 
+  "label": "Humans CR2" 
+ },
+ {"value": "main stairs b", 
+  "code": "MSB", 
+  "label": "Main Stairs B" 
+ },
+ {"value": "court", 
+  "code": "Court", 
+  "label": "Court" 
+ },
+ {"value": "court staircase basement", 
+  "code": "CSCB", 
+  "label": "Court staircase Basement" 
+ },
+ {"value": "b stairs (basement)", 
+  "code": "BSB", 
+  "label": "B Stairs (Basement)" 
+ },
+ {"value": "amphitheatre", 
+  "code": "HAB", 
+  "label": "Amphitheatre" 
+ },
+ {"value": "multipurpose room", 
+  "code": "HMPR", 
+  "label": "MultiPurpose Room" 
+ },
+ {"value": "multipurpose court", 
+  "code": "HMPC", 
+  "label": "MultiPurpose Court" 
+ },
+ {"value": "multipurpose hall", 
+  "code": "HMPH", 
+  "label": "MultiPurpose Hall" 
+ },
+ {"value": "rifle range", 
+  "code": "HRR", 
+  "label": "Rifle Range" 
+ },
+ {"value": "gym", 
+  "code": "H01LG", 
+  "label": "Gym" 
+ },
+ {"value": "h03lg", 
+  "code": "H03LG", 
+  "label": "H03LG" 
+ },
+ {"value": "female toilet (block h)", 
+  "code": "HFT", 
+  "label": "Female Toilet (Block H)" 
+ },
+ {"value": "male toilet (block h)", 
+  "code": "HMT", 
+  "label": "Male Toilet (Block H)" 
+ },
+ {"value": "parade square", 
+  "code": "HPS", 
+  "label": "Parade Square" 
+ },
+ {"value": "pe store", 
+  "code": "HPES", 
+  "label": "PE Store" 
+ },
+ {"value": "g stairs b", 
+  "code": "GSB", 
+  "label": "G Stairs B" 
+ },
+ {"value": "stair 1", 
+ "code": "DS", 
+ "label": "Stair 1" 
+},
+{"value": "d201", 
+ "code": "D201", 
+ "label": "D201" 
+},
+{"value": "d202", 
+ "code": "D202", 
+ "label": "D202" 
+},
+{"value": "d203", 
+ "code": "D203", 
+ "label": "D203" 
+},
+{"value": "d204", 
+ "code": "D204", 
+ "label": "D204" 
+},
+{"value": "d205", 
+ "code": "D205", 
+ "label": "D205" 
+},
+{"value": "d206", 
+ "code": "D206", 
+ "label": "D206" 
+},
+{"value": "l2 d m toliet ", 
+ "code": "DMT", 
+ "label": "L2 D M toliet " 
+},
+{"value": "entrance of lecture theatres", 
+ "code": "CLTs", 
+ "label": "Entrance of Lecture theatres" 
+},
+{"value": "l2 b stairs (left)", 
+ "code": "BS1", 
+ "label": "L2 B Stairs (Left)" 
+},
+{"value": "l2 b m toliet ", 
+ "code": "BMT1", 
+ "label": "L2 B M toliet " 
+},
+{"value": "b207", 
+ "code": "B207", 
+ "label": "B207" 
+},
+{"value": "b208", 
+ "code": "B208", 
+ "label": "B208" 
+},
+{"value": "b209", 
+ "code": "B209", 
+ "label": "B209" 
+},
+{"value": "b210", 
+ "code": "B210", 
+ "label": "B210" 
+},
+{"value": "b211", 
+ "code": "B211", 
+ "label": "B211" 
+},
+{"value": "l2 b f toilet", 
+ "code": "BFT1", 
+ "label": "L2 B F toilet" 
+},
+{"value": "b206", 
+ "code": "B206", 
+ "label": "B206" 
+},
+{"value": "b205", 
+ "code": "B205", 
+ "label": "B205" 
+},
+{"value": "b204", 
+ "code": "B204", 
+ "label": "B204" 
+},
+{"value": "b203", 
+ "code": "B203", 
+ "label": "B203" 
+},
+{"value": "b202", 
+ "code": "B202", 
+ "label": "B202" 
+},
+{"value": "b201", 
+ "code": "B201", 
+ "label": "B201" 
+},
+{"value": "l2 b m toilet 2", 
+ "code": "BMT2", 
+ "label": "L2 B M toilet 2" 
+},
+{"value": "l2 b stairs (right)", 
+ "code": "BS2", 
+ "label": "L2 B Stairs (right)" 
+},
+{"value": "l2 b f toilet 2", 
+ "code": "BFT2", 
+ "label": "L2 B F toilet 2" 
+},
+{"value": "l2 a stairs", 
+ "code": "AS2", 
+ "label": "L2 A Stairs" 
+},
+{"value": "general office", 
+ "code": "GO", 
+ "label": "General Office" 
+},
+{"value": "l2 e m toilet", 
+ "code": "EMT1", 
+ "label": "L2 E M toilet" 
+},
+{"value": "l2 e stairs (left)", 
+ "code": "ES1", 
+ "label": "L2 E stairs (left)" 
+},
+{"value": "e212", 
+ "code": "E212", 
+ "label": "E212" 
+},
+{"value": "e211", 
+ "code": "E211", 
+ "label": "E211" 
+},
+{"value": "e210", 
+ "code": "E210", 
+ "label": "E210" 
+},
+{"value": "e209", 
+ "code": "E209", 
+ "label": "E209" 
+},
+{"value": "e208", 
+ "code": "E208", 
+ "label": "E208" 
+},
+{"value": "e207", 
+ "code": "E207", 
+ "label": "E207" 
+},
+{"value": "e201", 
+ "code": "E201", 
+ "label": "E201" 
+},
+{"value": "e202", 
+ "code": "E202", 
+ "label": "E202" 
+},
+{"value": "e203", 
+ "code": "E203", 
+ "label": "E203" 
+},
+{"value": "e204", 
+ "code": "E204", 
+ "label": "E204" 
+},
+{"value": "e205", 
+ "code": "E205", 
+ "label": "E205" 
+},
+{"value": "e206", 
+ "code": "E206", 
+ "label": "E206" 
+},
+{"value": "l2 e m toilet 2", 
+ "code": "EMT2", 
+ "label": "L2 E M toilet 2" 
+},
+{"value": "l2 e stairs (right)", 
+ "code": "ES2", 
+ "label": "L2 E stairs (right)" 
+},
+{"value": "band room", 
+ "code": "G201", 
+ "label": "Band Room" 
+},
+{"value": "lecture theatre 1", 
+ "code": "LT1", 
+ "label": "Lecture Theatre 1" 
+},
+{"value": "blackbox", 
+ "code": "G202", 
+ "label": "Blackbox" 
+},
+{"value": "computer lab 1", 
+ "code": "J208", 
+ "label": "Computer Lab 1" 
+},
+{"value": "computer lab 2", 
+ "code": "J207", 
+ "label": "Computer Lab 2" 
+},
+{"value": "mep 1", 
+ "code": "J206", 
+ "label": "MEP 1" 
+},
+{"value": "mep 2", 
+ "code": "J205", 
+ "label": "MEP 2" 
+},
+{"value": "mep 3", 
+ "code": "J204", 
+ "label": "MEP 3" 
+},
+{"value": "mep 4", 
+ "code": "J203", 
+ "label": "MEP 4" 
+},
+{"value": "l2 j stairs 1", 
+ "code": "JS1", 
+ "label": "L2 J Stairs 1" 
+},
+{"value": "mep 5", 
+ "code": "J202", 
+ "label": "MEP 5" 
+},
+{"value": "j201", 
+ "code": "J201", 
+ "label": "J201" 
+},
+{"value": "l2 j stairs 2", 
+ "code": "JS2", 
+ "label": "L2 J Stairs 2" 
+},
+{"value": "l2 j f toilet", 
+ "code": "JFT", 
+ "label": "L2 J F Toilet" 
+},
+{"value": "l2 j m toilet", 
+ "code": "JMT", 
+ "label": "L2 J M Toilet" 
+},
+{"value": "auditorium", 
+ "code": "Audi", 
+ "label": "Auditorium" 
+},
+{"value": "seminar room", 
+ "code": "J209", 
+ "label": "Seminar Room" 
+},
+{"value": "d301", 
+ "code": "D301", 
+ "label": "D301" 
+},
+{"value": "l3 d stairs", 
+ "code": "DSL", 
+ "label": "L3 D Stairs" 
+},
+{"value": "d302", 
+ "code": "D302", 
+ "label": "D302" 
+},
+{"value": "d303", 
+ "code": "D303", 
+ "label": "D303" 
+},
+{"value": "d304", 
+ "code": "D304", 
+ "label": "D304" 
+},
+{"value": "d305", 
+ "code": "D305", 
+ "label": "D305" 
+},
+{"value": "d306", 
+ "code": "D306", 
+ "label": "D306" 
+},
+{"value": "l3 d f toilets", 
+ "code": "DTG", 
+ "label": "L3 D F Toilets" 
+},
+{"value": "dance room (left)", 
+ "code": "C302", 
+ "label": "Dance Room (Left)" 
+},
+{"value": "l3 b f toilet", 
+ "code": "BTBG", 
+ "label": "L3 B F Toilet" 
+},
+{"value": "dance room (right)", 
+ "code": "C303", 
+ "label": "Dance Room (Right)" 
+},
+{"value": "ip lab (lab 1)", 
+ "code": "B301", 
+ "label": "IP Lab (Lab 1)" 
+},
+{"value": "l3 b stairs (left)", 
+ "code": "BLS", 
+ "label": "L3 B Stairs (left)" 
+},
+{"value": "l3 b m toilet 1", 
+ "code": "BTBU", 
+ "label": "L3 B M Toilet 1" 
+},
+{"value": "balance room 1", 
+ "code": "B302", 
+ "label": "Balance Room 1" 
+},
+{"value": "preperation room 1", 
+ "code": "B303", 
+ "label": "Preperation Room 1" 
+},
+{"value": "ip lab (lab 2)", 
+ "code": "B305", 
+ "label": "IP Lab (Lab 2)" 
+},
+{"value": "physics lab (lab 1)", 
+ "code": "B304", 
+ "label": "Physics Lab (Lab 1)" 
+},
+{"value": "physics lab (lab 2)", 
+ "code": "B306", 
+ "label": "Physics Lab (Lab 2)" 
+},
+{"value": "l3 b m toilet 2", 
+ "code": "BTBL", 
+ "label": "L3 B M Toilet 2" 
+},
+{"value": "staff room", 
+ "code": "SU", 
+ "label": "Staff Room" 
+},
+{"value": "physics lab (lab 3)", 
+ "code": "B307", 
+ "label": "Physics Lab (Lab 3)" 
+},
+{"value": "l3 b stairs (right)", 
+ "code": "BRS", 
+ "label": "L3 B Stairs (right)" 
+},
+{"value": "l3 b m toilet (right)", 
+ "code": "BTBR", 
+ "label": "L3 B M Toilet (right)" 
+},
+{"value": "staff pantry", 
+ "code": "SP", 
+ "label": "Staff Pantry" 
+},
+{"value": "l3 e f toilet", 
+ "code": "ETW", 
+ "label": "L3 E F Toilet" 
+},
+{"value": "biology lab (lab 2)", 
+ "code": "E305", 
+ "label": "Biology Lab (Lab 2)" 
+},
+{"value": "l3 e stairs (left)", 
+ "code": "ELS", 
+ "label": "L3 E Stairs (left)" 
+},
+{"value": "l3 e f toilet 1", 
+ "code": "ETGD", 
+ "label": "L3 E F Toilet 1" 
+},
+{"value": "chemisty lab (lab 2)", 
+ "code": "E306", 
+ "label": "Chemisty Lab (Lab 2)" 
+},
+{"value": "biology lab (lab 1)", 
+ "code": "E301", 
+ "label": "Biology Lab (Lab 1)" 
+},
+{"value": "chemistry lab (lab 3)", 
+ "code": "E307", 
+ "label": "Chemistry Lab (Lab 3)" 
+},
+{"value": "preperation room 2", 
+ "code": "E302", 
+ "label": "Preperation Room 2" 
+},
+{"value": "balance room 2", 
+ "code": "ELS", 
+ "label": "Balance Room 2" 
+},
+{"value": "l3 e f toilet 2", 
+ "code": "ETGU", 
+ "label": "L3 E F Toilet 2" 
+},
+{"value": "chemisty lab (lab 1)", 
+ "code": "E304", 
+ "label": "Chemisty Lab (Lab 1)" 
+},
+{"value": "l3 e stairs (right)", 
+ "code": "ERS", 
+ "label": "L3 E Stairs (right)" 
+},
+{"value": "computer lab 3", 
+ "code": "G301", 
+ "label": "Computer Lab 3" 
+},
+{"value": "l3 e m toilet", 
+ "code": "ETB", 
+ "label": "L3 E M Toilet" 
+},
+{"value": "j308", 
+ "code": "J308", 
+ "label": "J308" 
+},
+{"value": "library", 
+ "code": "LIB", 
+ "label": "Library" 
+},
+{"value": "lib room 1", 
+ "code": "J301", 
+ "label": "Lib Room 1" 
+},
+{"value": "lib room 5", 
+ "code": "J307", 
+ "label": "Lib Room 5" 
+},
+{"value": "lib room 2", 
+ "code": "J302", 
+ "label": "Lib Room 2" 
+},
+{"value": "maker space", 
+ "code": "J306", 
+ "label": "Maker Space" 
+},
+{"value": "lib room 3", 
+ "code": "J303", 
+ "label": "Lib Room 3" 
+},
+{"value": "ict/av office", 
+ "code": "J305", 
+ "label": "ICT/AV Office" 
+},
+{"value": "lib room 4", 
+ "code": "J304", 
+ "label": "Lib Room 4" 
+},]
